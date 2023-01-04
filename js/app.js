481d33c66f284316fe0f0f6d1a59079f708ed515
MatthewChong <chiongmatthew96@gmail.com>
2023-01-04 21:00:50 +0800

    commit easybank

---
+++ js/app.js
@@ -0,0 +1,21 @@
+const hamburger = document.getElementById("hamburger");
+const navBar = document.getElementById("header-nav-container");
+const overlay = document.getElementById("overlay");
+const intro = document.getElementById("intro");
+const navLinks = document.querySelectorAll(".header-nav-links");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navBar.classList.toggle("active");
+  overlay.classList.toggle("active");
+  intro.classList.toggle("active");
+});
+
+navLinks.forEach((links) => {
+  links.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navBar.classList.toggle("active");
+    overlay.classList.toggle("active");
+    intro.classList.toggle("active");
+  });
+});
